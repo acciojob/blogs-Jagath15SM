507c82777a2a47f93662d8c6bba5f47d611373a8
JagathSM <jagathsingularity@gmail.com>
2023-01-14 19:25:24 +0530

    Blogs

--- src/main/java/com/driver/models/Image.java
+++ src/main/java/com/driver/models/Image.java
@@ -33,6 +33,10 @@ public class Image {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getDescription() {
         return description;
     }
