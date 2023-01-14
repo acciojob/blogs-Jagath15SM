58f843f8fe9527c219b681a0c575d3281f9791c1
JagathSM <jagathsingularity@gmail.com>
2023-01-14 16:31:39 +0530

    Blogs

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -22,31 +22,56 @@ public class BlogService {
     ImageService imageService1;
 
     @Autowired
+    ImageRepository imageRepository;
+
+    @Autowired
     UserRepository userRepository1;
 
     public List<Blog> showBlogs(){
         //find all blogs
-
+        return new ArrayList<>();
     }
 
     public void createAndReturnBlog(Integer userId, String title, String content) {
         //create a blog at the current time
+        Blog blog = new Blog();
+        blog.setTitle(title);
+        blog.setContent(content);
 
         //updating the blog details
+        blogRepository1.save(blog);
 
         //Updating the userInformation and changing its blogs
-
+        User user = userRepository1.findById(userId).get();
+        user.getBlogList().add(blog);
+        userRepository1.save(user);
     }
 
     public Blog findBlogById(int blogId){
-        //find a blog
+        Blog blog = blogRepository1.findById(blogId).get();
+        return blog;
     }
 
     public void addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog after creating it
+        Image image = new Image();
+        image.setDescription(description);
+        image.setDimensions(dimensions);
+
+        imageRepository.save(image);
+
+
+        Blog blog = blogRepository1.findById(blogId).get();
+        blog.getImageList().add(image);
+        blogRepository1.save(blog);
+
     }
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
+        Blog blog = blogRepository1.findById(blogId).get();
+        blog.getImageList().clear();
+
+        blogRepository1.deleteById(blogId);
     }
 }
